008a9fca08e3dbf8b5ced8b92045ec4536939e91
Israel Levin <israel@paket.global>
2018-09-16 14:29:59 +0300

    Use ports from env in kill script

--- kill.sh
+++ kill.sh
@@ -1,6 +1,17 @@
 #!/bin/bash
 # Stop the PAKET servers (or any python servers listening on same ports).
 
+. paket.env
+
+
+[ "$1" ] || "$0" ${PAKET_ROUTER_PORT:-5000} ${PAKET_BRIDGE_PORT:-5001} ${PAKET_FUNDER_PORT:-5002}
+while [ "$1" ]; do
+    pid=$(netstat -lntp | grep -F "0.0.0.0:$1 " | grep -Po '\d*(?=/)')
+    [ "$pid" ] && kill $pid || echo "no process listening on port $1"
+    shift
+done
+PAKET_BRIDGE_PORT=5001
+PAKET_FUNDER_PORT=5002
 [ "$1" ] || "$0" 5000 5001 5002
 while [ "$1" ]; do
     pid=$(netstat -lntp | grep -F "0.0.0.0:$1 " | grep -Po '\d*(?=/)')
