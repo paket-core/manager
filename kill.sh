667b6ca70e9d66104facd8de59568a7b5e46fe4d
Israel Levin <israel@paket.global>
2018-10-03 11:42:32 +0300

    Fix kill script

--- kill.sh
+++ kill.sh
@@ -3,16 +3,7 @@
 
 . paket.env
 
-
 [ "$1" ] || "$0" ${PAKET_ROUTER_PORT:-5000} ${PAKET_BRIDGE_PORT:-5001} ${PAKET_FUNDER_PORT:-5002}
-while [ "$1" ]; do
-    pid=$(netstat -lntp | grep -F "0.0.0.0:$1 " | grep -Po '\d*(?=/)')
-    [ "$pid" ] && kill $pid || echo "no process listening on port $1"
-    shift
-done
-PAKET_BRIDGE_PORT=5001
-PAKET_FUNDER_PORT=5002
-[ "$1" ] || "$0" 5000 5001 5002
 while [ "$1" ]; do
     pid=$(netstat -lntp | grep -F "0.0.0.0:$1 " | grep -Po '\d*(?=/)')
     [ "$pid" ] && kill $pid || echo "no process listening on port $1"
