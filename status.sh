02581d57d46d44126b0daff22b17e92f0ce3f6ac
Israel Levin <israel@paket.global>
2018-10-03 11:42:48 +0300

    Add git state script

---
+++ status.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# List git status of PAKET servers.
+
+. paket.env
+
+for server in "${PAKET_SERVERS[@]}"; do
+    echo "$server: $(git -C "../$server" rev-parse HEAD)"
+    while read requirement; do
+        if [ "${requirement:0:3}" = '../' ]; then
+            echo "${requirement:3}: $(git -C "$requirement" rev-parse HEAD)"
+        fi
+    done < "../$server/requirements.txt"
+done | sort -u | column -t
