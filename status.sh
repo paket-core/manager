a663243dedfc6537eb0f5fce5b87e54e669ccef7
Israel Levin <israel@paket.global>
2018-12-05 15:12:12 +0200

    Do it properly

--- status.sh
+++ status.sh
@@ -3,11 +3,24 @@
 
 . paket.env
 
-for server in "${PAKET_SERVERS[@]}"; do
-    echo "$server: $(git -C "../$server" rev-parse HEAD) $(git -C "../$server" symbolic-ref HEAD)"
+git_stat() {
+    exec 6<&1
+    exec 1>/dev/null
+    pushd "$1"
+    commit=$(git rev-parse HEAD)
+    branch=$(git symbolic-ref HEAD)
+    git status --porcelain | grep -v '^??' && branch=dirty
+    echo "$(pwd): $commit $branch" 1>&6
+    popd
+    exec 1<&6
+}
+
+
+for server in "${PAKET_SERVERS[@]}" manager; do
+    git_stat "../$server"
     while read requirement; do
         if [ "${requirement:0:3}" = '../' ]; then
-            echo "${requirement:3}: $(git -C "$requirement" rev-parse HEAD)"
+            git_stat "$requirement"
         fi
     done < "../$server/requirements.txt"
 done | sort -u | column -t
