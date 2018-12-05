c2dbe757281d4ed0c002d4acf9ee401f0a0e40d9
Israel Levin <israel@paket.global>
2018-12-05 14:55:40 +0200

    Add branch name to status

--- status.sh
+++ status.sh
@@ -4,7 +4,7 @@
 . paket.env
 
 for server in "${PAKET_SERVERS[@]}"; do
-    echo "$server: $(git -C "../$server" rev-parse HEAD)"
+    echo "$server: $(git -C "../$server" rev-parse HEAD) $(git -C "../$server" symbolic-ref HEAD)"
     while read requirement; do
         if [ "${requirement:0:3}" = '../' ]; then
             echo "${requirement:3}: $(git -C "$requirement" rev-parse HEAD)"
