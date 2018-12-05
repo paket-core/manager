6181e157f4936558653f445a7be85e133aae8d4e
Israel Levin <israel@paket.global>
2018-12-05 15:18:24 +0200

    Check for unpushed commits

--- status.sh
+++ status.sh
@@ -8,8 +8,12 @@ git_stat() {
     exec 1>/dev/null
     pushd "$1"
     commit=$(git rev-parse HEAD)
-    branch=$(git symbolic-ref HEAD)
-    git status --porcelain | grep -v '^??' && branch=dirty
+    branch=$(git symbolic-ref HEAD | cut -d'/' -f3)
+    if git status --porcelain | grep -v '^??'; then
+        branch=dirty
+    elif git log origin/$branch..HEAD | grep '^commit'; then
+        branch=unpushed
+    fi
     echo "$(pwd): $commit $branch" 1>&6
     popd
     exec 1<&6
